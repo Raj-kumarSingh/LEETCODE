2b8cfde91436f96c4dd888548f75b67eda37e147
Raj-kumarSingh <rajs93138@gmail.com>
2024-04-19 02:26:57 +0530

    1st Occur

---
+++ firstOccurance.java
@@ -0,0 +1,34 @@
+public class firstOccurance {
+    public boolean Compare(String haystack, String needle, int idx)
+    {
+        int n1=haystack.length();
+        int n2=needle.length();
+        for(int i=0;i<n2;i++)
+        {
+            if(idx>=n1)
+            {
+                return false;
+            }
+            if(haystack.charAt(idx++)!=needle.charAt(i))
+            {
+                return false;
+            }
+        }
+        return true;
+
+    }
+    public int strStr(String haystack, String needle) {
+        int n1=haystack.length();
+        for(int i=0;i<n1;i++)
+        {
+            if(haystack.charAt(i)==needle.charAt(0))
+            {
+                if(Compare(haystack,needle,i)==true)
+                {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+}
